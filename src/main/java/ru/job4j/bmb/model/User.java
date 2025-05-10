2525e44bf3fd7d09554a13cb0f418117f2a9d91d
underMichael <m.podddorogin@gmail.com>
2025-05-10 14:29:10 +0300

    Revert "Spring Data JPA"
    This reverts commit e521b9b990bda0f298691ce959b02ea56e9ce4f4.

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,37 +1,29 @@
 package ru.job4j.bmb.model;
 
-import jakarta.persistence.*;
-
 import java.util.Objects;
 
-@Entity
-@Table(name = "mb_user")
 public class User {
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
-
-		@Column(name = "client_id")
 		private long clientId;
-
-		@Column(name = "chat_id")
 		private long chatId;
 
 		@Override
 		public boolean equals(Object o) {
-				if (this == o) {
+				if (o == this) {
 						return true;
 				}
 				if (o == null || getClass() != o.getClass()) {
 						return false;
 				}
 				User user = (User) o;
-				return Objects.equals(id, user.id);
+				return clientId == user.clientId
+						&& chatId == user.chatId
+						&& Objects.equals(id, user.id);
 		}
 
 		@Override
 		public int hashCode() {
-				return Objects.hash(id);
+				return Objects.hash(id, clientId, chatId);
 		}
 
 		public Long getId() {
