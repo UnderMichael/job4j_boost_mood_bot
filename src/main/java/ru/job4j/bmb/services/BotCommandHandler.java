ef96115165f2119e4d8d3574b3d544aa9c3fab69
underMichael <m.podddorogin@gmail.com>
2025-05-11 12:47:17 +0300

    @Service и @Repository | BotCommandHandler - обработка комманд

--- src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
@@ -1,13 +1,50 @@
 package ru.job4j.bmb.services;
 
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.job4j.bmb.model.Content;
+import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class BotCommandHandler {
+		private final UserRepository userRepository;
+		private final MoodService moodService;
+		private final TgUI tgUI;
 
-		void receive(Content content) {
-				System.out.println(content);
+		public BotCommandHandler(UserRepository userRepository, MoodService moodService, TgUI tgUI) {
+				this.userRepository = userRepository;
+				this.moodService = moodService;
+				this.tgUI = tgUI;
 		}
 
+		Optional<Content> commands(Message message) {
+				return switch (message.getText()) {
+						case "/start" -> handleStartCommand(message.getChatId(), message.getFrom().getId());
+						case "/week_mood_log" -> moodService.weekMoodLogCommand(message.getChatId(), message.getFrom().getId());
+						case "/month_mood_log" -> moodService.monthMoodLogCommand(message.getChatId(), message.getFrom().getId());
+						case "/award" -> moodService.awards(message.getChatId(), message.getFrom().getId());
+						default -> Optional.empty();
+				};
+		}
+
+		Optional<Content> handleCallback(CallbackQuery callback) {
+				var moodId = Long.valueOf(callback.getData());
+				var user = userRepository.findById(callback.getFrom().getId());
+				return user.map(value -> moodService.chooseMood(value, moodId));
+		}
+
+		private Optional<Content> handleStartCommand(long chatId, Long clientId) {
+				var user = new User();
+				user.setClientId(clientId);
+				user.setChatId(chatId);
+				userRepository.save(user);
+				var content = new Content(user.getChatId());
+				content.setText("Как настроение?");
+				content.setMarkup(tgUI.buildButtons());
+				return Optional.of(content);
+		}
 }
